bba21aac1201671edbcba359b0fe956f78501017
Duy Thanh <95486959+trduyTh4nh@users.noreply.github.com>
2023-07-17 22:13:32 +0700

    deploy project flysite

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/https://github.com/trduyTh4nh/FlySite/' // Thay tên repository của các bạn vào đây nhé
+      : '/'
+  }
+  
+  
